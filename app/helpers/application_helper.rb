9ebbe48d102515e0f887ddea05be59d4c0950c90
keighty <k80.leonard@gmail.com>
2013-07-04 15:50:39 -0700

    adds full_title method to application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(page_title)
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "Social Bridge"
     if page_title.empty?
       base_title
     else
