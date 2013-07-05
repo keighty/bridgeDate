013dced46ed2debe67f550a4d8f5ecbbe697a6a7
keighty <k80.leonard@gmail.com>
2013-07-04 18:36:06 -0700

    adds tests for user profile page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -9,10 +9,10 @@ describe "User pages" do
   end
 
   describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
 
-    # it { should have_selector('h1', text: user.name) }
-    # it { should have_selector('title', text: user.name) }
-
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_title(user.name) }
   end
 end
