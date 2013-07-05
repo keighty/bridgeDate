ca7cd0421277aaa0aacda1f18b1d40b251354098
keighty <k80.leonard@gmail.com>
2013-07-05 07:53:32 -0700

    adds flash success message, removes notice message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        flash[:success] = "Welcome to Bridge Date!"
+        format.html { redirect_to @user }
         format.json { render action: 'show', status: :created, location: @user }
       else
         format.html { render action: 'new' }
