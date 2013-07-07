c5c3607a77893c60eaefd1dcc4af461aa43c8414
keighty <k80.leonard@gmail.com>
2013-07-07 09:11:53 -0700

    adds session routes REST

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Bridge::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root to: 'pages#home'
   match '/help', to: 'pages#help', via: 'get'
   match '/about', to: 'pages#about', via: 'get'
 
   match '/signup', to: 'users#new', via: 'get'
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
+
 end
