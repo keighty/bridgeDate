cc516ca5e16ef6488e9f4c7d49f76971ea8ee5c2
keighty <k80.leonard@gmail.com>
2013-07-07 10:36:41 -0700

    adds session token logic to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
-  # attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
 
   before_save { email.downcase! }
+  before_create :create_remember_token
 
   validates :name, presence: true, length: { maximum: 50 }
 
@@ -14,4 +14,17 @@ class User < ActiveRecord::Base
                     length: { minimum: 6 }
   validates :password_confirmation,
                     presence: true
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
