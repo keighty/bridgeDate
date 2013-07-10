89cf7cadd1a2e484b9682b458009a2142d4ab5ed
keighty <k80.leonard@gmail.com>
2013-07-09 18:31:01 -0700

    adds sign in logic for tests to work

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,15 @@
 include ApplicationHelper
+
+def sign_in(user, options={})
+  if options[:no_capybara]
+    # sign in when not using no_capybara
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
+  else
+    visit signin_path
+    fill_in "Email",    with: user.Email
+    fill_in "Password", with: user.Password
+    click_button "Sign in"
+  end
+end
