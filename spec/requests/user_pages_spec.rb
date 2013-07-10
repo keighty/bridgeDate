bc1740382e65337f92b361cc7a9396d33b5ac607
keighty <k80.leonard@gmail.com>
2013-07-09 18:28:32 -0700

    adds tests for edit user

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -32,12 +32,29 @@ describe "User pages" do
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "Confirm Password", with: "foobar"
       end
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+    end
+  end
+
+  describe 'edit' do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit edit_user_path(user) }
+
+    describe 'page' do
+      it { should have_content("Update your profile") }
+      it { should have_title("Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
     end
   end
 end
