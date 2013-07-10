ad9308dd9e9c93b146e4f3efa739f8f5cbe51261
keighty <k80.leonard@gmail.com>
2013-07-09 18:27:56 -0700

    adds tests for login with valid information

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -8,7 +8,6 @@ describe "Authentication" do
 
     it { should have_content('Sign in') }
     it { should have_title('Sign in') }
-
   end
 
   describe "signin" do
@@ -39,5 +38,16 @@ describe "Authentication" do
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
     end
+  end
+
+  describe "with valid information" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { sign_in user }
+
+    it { should have_title(user.name) }
+    it { should have_link('Profile',      href: user_path(user))  }
+    it { should have_link('Settings',      href: edit_user_path(user))  }
+    it { should have_link('Sign out',      href: signout_path)  }
+    it { should_not have_link('Sign in',      href: signin_path)  }
   end
 end
