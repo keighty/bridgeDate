4d51b7106357a2f0a041e05ac4a7391e3a969a1c
keighty <k80.leonard@gmail.com>
2013-07-11 14:29:26 -0700

    adds remember token to current user definition

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        sign_in @user
         flash[:success] = "Welcome to Bridge Date!"
         format.html { redirect_to @user }
         format.json { render action: 'show', status: :created, location: @user }
