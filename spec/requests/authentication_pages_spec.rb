2ebf0365f4b969590c0a05b88a249770cf239652
keighty <k80.leonard@gmail.com>
2013-07-11 14:31:56 -0700

    adds tests for authentication with valid info

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -13,41 +13,30 @@ describe "Authentication" do
   describe "signin" do
     before { visit signin_path }
 
-      describe "with invalid information" do
-        before { click_button "Sign in" }
+    describe "with invalid information" do
+      before { click_button "Sign in" }
 
-        it { should have_title('Sign in') }
-        it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+      it { should have_title('Sign in') }
+      it { should have_selector('div.alert.alert-error', :text => 'Invalid') }
+    end
 
-      describe "after visiting another page" do
-        before { click_link "Home" }
-        it { should_not have_selector('div.alert.alert-error') }
-      end
+    describe "after visiting another page" do
+      before { click_link "Home" }
+      it { should_not have_selector('div.alert.alert-error') }
     end
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
+        fill_in "Email", :with => user.email.upcase
+        fill_in "Password", :with => user.password
         click_button "Sign in"
       end
 
       it { should have_title(user.name) }
-      it { should have_link('Profile',     href: user_path(user)) }
-      it { should have_link('Sign out',    href: signout_path) }
-      it { should_not have_link('Sign in', href: signin_path) }
+      it { should have_link('Profile', :href => user_path(user)) }
+      it { should have_link('Sign out', :href => signout_path) }
+      it { should_not have_link('Sign in', :href => signin_path) }
     end
-  end
-
-  describe "with valid information" do
-    let(:user) { FactoryGirl.create(:user) }
-    before { sign_in user }
-
-    it { should have_title(user.name) }
-    it { should have_link('Profile',      href: user_path(user))  }
-    it { should have_link('Settings',      href: edit_user_path(user))  }
-    it { should have_link('Sign out',      href: signout_path)  }
-    it { should_not have_link('Sign in',      href: signin_path)  }
   end
 end
