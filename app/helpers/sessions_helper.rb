0c7b84e7d7437ee35e89c08912e34e82f1a268a9
keighty <k80.leonard@gmail.com>
2013-07-13 14:47:55 -0700

    adds redirect_back_to method to helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -23,6 +23,15 @@ module SessionsHelper
     user == current_user
   end
 
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url
+  end
+
   def sign_out
     self.current_user = nil
     cookies.delete(:remember_token)
