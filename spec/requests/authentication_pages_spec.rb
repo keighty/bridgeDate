1a6ca451cdc3fef027da4eed3a8e060d1cdf69bb
keighty <k80.leonard@gmail.com>
2013-07-13 13:50:14 -0700

    adds test for ensuring correct user is signed in for edit action

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -62,5 +62,21 @@ describe "Authentication" do
         end
       end
     end
+
+    describe "as wrong user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com")}
+      before { sign_in user, no_capybara: true }
+
+      describe "visiting Users#edit page" do
+        before { visit edit_user_path(wrong_user) }
+        it { should_not have_title(full_title('Edit user')) }
+      end
+
+      describe "submitting a PATCH request to the Users#update action" do
+        before { patch user_path(wrong_user) }
+        specify { expect(response).to redirect_to(root_path) }
+      end
+    end
   end
 end
