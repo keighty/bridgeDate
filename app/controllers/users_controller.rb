c3a01d600310b2f8e28bf5e567f37ec902fa4fb1
keighty <k80.leonard@gmail.com>
2013-07-13 14:47:31 -0700

    adds redirect_back_to method to helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user,  only: [:edit, :update]
+  before_action :signed_in_user,  only: [:index, :edit, :update]
   before_action :correct_user,    only: [:edit, :update]
 
   def index
@@ -69,7 +69,10 @@ class UsersController < ApplicationController
     end
 
     def signed_in_user
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
     end
 
     def correct_user
