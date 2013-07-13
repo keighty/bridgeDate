98d298dd277da559d92b1b1ef936c00ad42eb63e
keighty <k80.leonard@gmail.com>
2013-07-13 14:49:39 -0700

    adds test for viewing all users

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -51,7 +51,7 @@ describe "User pages" do
     end
   end
 
-  describe 'edit' do
+  describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
     before do
       # debugger
@@ -87,6 +87,24 @@ describe "User pages" do
       it { should have_link('Sign out', href: signout_path)}
       specify { expect(user.reload.name).to eq new_name }
       specify { expect(user.reload.email).to eq new_email }
+    end
+  end
+
+  describe "index" do
+    before do
+      sign_in FactoryGirl.create(:user)
+      FactoryGirl.create(:user, name: 'Bob', email: 'bob@example.com')
+      FactoryGirl.create(:user, name: 'Ben', email: 'ben@example.com')
+      visit users_path
+    end
+
+    it { should have_title('All users') }
+    it { should have_content('All users') }
+
+    it "should list each user" do
+      User.all.each do |user|
+        expect(page).to have_selector('li', text: user.name)
+      end
     end
   end
 end
