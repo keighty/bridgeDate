ba07227bce36601a086c3aec30b68e02be0ff043
keighty <k80.leonard@gmail.com>
2013-07-13 14:56:03 -0700

    adds test, style, and link to users index

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -34,8 +34,10 @@ describe "Authentication" do
       end
 
       it { should have_title(user.name) }
+      it { should have_link('Users', :href => users_path) }
       it { should have_link('Profile', :href => user_path(user)) }
       it { should have_link('Sign out', :href => signout_path) }
+      it { should have_link('Settings', :href => edit_user_path(user)) }
       it { should_not have_link('Sign in', :href => signin_path) }
 
       describe "followed by signout" do
