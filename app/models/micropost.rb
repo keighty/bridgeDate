c55768e2effc24124592f6a0522e12654e97ae3c
keighty <k80.leonard@gmail.com>
2013-07-21 20:08:23 -0700

    adds tests and views to display microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,5 @@ class Micropost < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order('created_at DESC') }
   validates :user_id, presence: true
+  validates :content, presence: true, length: { maximum: 140 }
 end
